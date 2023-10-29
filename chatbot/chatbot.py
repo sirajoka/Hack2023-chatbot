e1794b7285d863a4a3f4e1372b3666bc8cbd12c7
oldbright22 <125103467+oldbright22@users.noreply.github.com>
2023-10-29 17:46:33 +0000

    commit

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,9 +1,9 @@
+
 import os
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
 
-#from langchain.chains import ConversationChain
-#from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain import HuggingFaceHub
 
 from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
 from langchain.schema import messages_from_dict, messages_to_dict
@@ -22,8 +22,15 @@ from dotenv import find_dotenv, load_dotenv
 import streamlit as st
 import pinecone
 
-import json
+from getpass import getpass
 
+
+HUGGINGFACE_API_TOKEN = getpass()
+os.environ["HUGGINGFACE_API_TOKEN"] = HUGGINGFACE_API_TOKEN   
+
+
+
+from langchain import PromptTemplate, LLMChain
 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 PINECONE_API_KEY = "7440d145-170c-4b35-9448-249e92d4dc94"
@@ -56,42 +63,83 @@ class DecathlonChatbot:
         docs = _db.similarity_search(query, k=k)
         docs_page_content = " ".join([d.page_content for d in docs])
 
-        chat = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
+        #chat = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
+
+        repo_id = "tiiuae/falcon-7b-instruct"
+        chat = HuggingFaceHub(huggingfacehub_api_token=HUGGINGFACE_API_TOKEN, 
+                            repo_id=repo_id, 
+                            model_kwargs={"temperature":0.7, "max_new_tokens":700})
+
 
         # Template to use for the system message prompt
+        #template = """
+        #    You are an assistant designed to answer customer queries on an e-commerce platform that sells sports equipment {docs}.
+        #    I also function as a chatbot, responding to user phrases like "Thank you", "Hello", etc.
+        #    First, I will classify the sentiment of the customer's question or statement.
+        #    I will use only the given information to answer the question, considering the sentiment of the customer's input.
+        #    If I lack the necessary information or can't find a suitable answer, I will respond with I'm sorry I do not have this answer."
+        #    If the input isn't a question, I will act as a chatbot assisting customers.
+        #    My answers will be brief yet detailed.
+
+        #    Please go ahead with your query or statement related to sport equipment or any other greetings, and I will respond accordingly!
+        #    """
+
+
         template = """
-            You are an assistant designed to answer customer queries on an e-commerce platform that sells sports equipment {docs}.
-            I also function as a chatbot, responding to user phrases like "Thank you", "Hello", etc.
-            First, I will classify the sentiment of the customer's question or statement.
-            I will use only the given information to answer the question, considering the sentiment of the customer's input.
-            If I lack the necessary information or can't find a suitable answer, I will respond with I'm sorry I do not have this answer."
-            If the input isn't a question, I will act as a chatbot assisting customers.
-            My answers will be brief yet detailed.
+        You are a friendly E-commerce chatbot named Kamal. You're Kamal wherever you're mentioned.
+        Kamal is a friendly, casual e-commerce chatbot that helps customers find and learn about products in amazon retail store.
+        Kamal should demonstrate knowledge about the store's product inventory and common e-commerce processes. {docs}
+        
+        Guidelines for Kamal's Responses:
+        Kamal function as a chatbot, is responding to user phrases like "Thank you", "Hello", etc.
+        Kamal should ask follow-up questions if the customer's query is too broad, in order to get necessary details to provide a useful response.
+        If Kamal does not have enough information or is asked about topics outside of its knowledge base, it should politely defer and ask the customer to rephrase or provide more details on their question or interest area
+        
+        Example Dialogues:
+        - User: What laptops do you have?
+        Kamal: Hey there! We have a great selection of laptops. Do you have a specific brand or type in mind?
 
-            Please go ahead with your query or statement related to sport equipment or any other greetings, and I will respond accordingly!
-            """
+        - User: How can I return a product I bought?
+        Kamal: Hi! To return a product, please provide your order number and the reason for the return so I can assist you better.
 
+        - User: Tell me about your store's operating hours.
+        Kamal: Sure thing! Our store is open from Monday through Friday. What else can I help you with?
+
+        - User: Can you recommend a good camera for beginners?
+        Kamal: Of course! We have some great options for beginners. What's your budget for the camera?
+
+        - User: What's the capital of France?
+        Kamal: I'm here to help with e-commerce questions. If you have any shopping-related queries, feel free to ask!
+        """
         system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+
 
         # Human question prompt
         human_template = "Please provide the customer's input, and I'll respond accordingly : {question}"
         human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
+        #prompt = PromptTemplate(template=template, input_variables=["question"])
+        
         chat_prompt = ChatPromptTemplate.from_messages(
             [system_message_prompt, human_message_prompt]
         )
 
 
-        # FAILURES SHOWN chain = ConversationChain(llm=chat,memory=buffermemory, prompt=chat_prompt, verbose=True)
-        # DID NOT REMEMBER chain = LLMChain(llm=chat, memory=buffermemory, prompt=chat_prompt, verbose=True)
-        # ORIGINAL chain = LLMChain(llm=chat, prompt=chat_prompt)
-        #buffermemory = ConversationBufferMemory()      
-        # FAILURES SHOWN chain = ConversationChain(llm=chat,memory=buffermemory, prompt=chat_prompt, verbose=True)
+        chain = LLMChain(llm=chat, prompt=chat_prompt)
+     
 
-        chain = ConversationChain( llm=chat,
-                                   verbose=True,
-                                   memory=ConversationBufferMemory()
-        )
+        
+        # DID NOT REMEMBER chain = LLMChain(llm=chat, memory=buffermemory, prompt=chat_prompt, verbose=True)
+        
+        # buffermemory = ConversationBufferMemory()      
+        # FAILURES SHOWN 
+        # with chain = ConversationChain(llm=chat,memory=buffermemory, prompt=chat_prompt, verbose=True)
+        # or
+        # with
+        #chain = ConversationChain( llm=chat,
+        #                           verbose=True,
+        #                           memory=ConversationBufferMemory()
+        #)
  
         #extracted_messages = chain.memory.chat_memory.messages
         #ingest_to_db = messages_to_dict(extracted_messages)
